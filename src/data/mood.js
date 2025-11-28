b6c45f15fdd26d59cc0a074fd365f257e9a33360
brsujit <34603102+pmission@users.noreply.github.com>
2025-11-28 08:53:44 +0530

    Create mood.js

---
+++ src/data/mood.js
@@ -0,0 +1,8 @@
+export const MOODS = [
+  { label: "Anxious", color: "bg-blue-100 text-blue-800", target: 2 },
+  { label: "Angry", color: "bg-red-100 text-red-800", target: 16 },
+  { label: "Confused", color: "bg-purple-100 text-purple-800", target: 1 },
+  { label: "Demotivated", color: "bg-yellow-100 text-yellow-800", target: 3 },
+  { label: "Grieving", color: "bg-gray-200 text-gray-800", target: 2 },
+  { label: "Lonely", color: "bg-pink-100 text-pink-800", target: 9 }
+];
